23e1d6a4914915af5a28347766b8d52371c4a32d
Mashrafie Rahim Sheikh <rahimsheikh05@gmail.com>
2025-01-14 14:43:16 +0600

    fixed request in paypal complete order

--- src/app/modules/paypal/paypal.route.ts
+++ src/app/modules/paypal/paypal.route.ts
@@ -4,6 +4,6 @@ import auth from '../../middlewares/auth';
 const router = express.Router();
 
 router.post('/pay', auth(), paypalControllers.createPaymentSession);
-router.get('/complete-order', paypalControllers.completeOrder);
+router.patch('/complete-order', auth(), paypalControllers.completeOrder);
 
 export const paypalRouter = router;
