ce2238af820ce965976f4146ffb487cfea007df4
Mashrafie Rahim Sheikh <rahimsheikh05@gmail.com>
2025-02-04 17:06:52 +0600

    added sponsor status to jwt

--- src/app/utils/generateToken.ts
+++ src/app/utils/generateToken.ts
@@ -1,12 +1,19 @@
-import jwt, { Secret } from "jsonwebtoken";
+import { SponsorStatus } from '@prisma/client';
+import jwt, { Secret } from 'jsonwebtoken';
 
 export const generateToken = (
-  payload: { id: string; name: string; email: string; role: string },
+  payload: {
+    id: string;
+    name: string;
+    email: string;
+    role: string;
+    sponsorStatus: SponsorStatus;
+  },
   secret: Secret,
-  expiresIn: string
+  expiresIn: string,
 ) => {
   const token = jwt.sign(payload, secret, {
-    algorithm: "HS256",
+    algorithm: 'HS256',
     expiresIn: expiresIn,
   });
   return token;
