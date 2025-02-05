de7872181897c12265c287e65925e746a5c2e69f
Mashrafie Rahim Sheikh <rahimsheikh05@gmail.com>
2025-02-05 12:39:57 +0600

    updated notification fields

--- src/app/modules/notification/notification.service.ts
+++ src/app/modules/notification/notification.service.ts
@@ -4,7 +4,16 @@ const getNotifications = async () => {
   const today = new Date();
   const day = today.getUTCDate();
   const month = today.getUTCMonth();
-  const users = await prisma.user.findMany({});
+  const users = await prisma.user.findMany({
+    select: {
+      id: true,
+      firstName: true,
+      lastName: true,
+      avatarUrl: true,
+      dob: true,
+      createdAt: true,
+    },
+  });
   const birthdayNotifications = users.filter(user => {
     const userDob = new Date(user.dob);
     const userDay = userDob.getUTCDate();
