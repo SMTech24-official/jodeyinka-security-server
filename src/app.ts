8864c3f7487d709a6e1d4df1af7aedd05b1d732b
Mashrafie Rahim Sheikh <rahimsheikh05@gmail.com>
2025-03-02 08:40:40 +0600

    fix body data limit to 5gb

--- src/app.ts
+++ src/app.ts
@@ -21,8 +21,8 @@ app.use(
 );
 
 //parser
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '5000mb' }));
+app.use(express.urlencoded({ limit: '5000mb', extended: true }));
 
 app.get('/', (req: Request, res: Response) => {
   res.send({
