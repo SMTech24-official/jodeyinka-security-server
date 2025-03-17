2fa1e9e3f4a23c9ddb464b2767f99e9e751686ec
Mashrafie Rahim Sheikh <rahimsheikh05@gmail.com>
2025-03-17 14:00:28 +0600

    remove authentication middleware from complete-order route

--- src/app/modules/paypal/paypal.route.ts
+++ src/app/modules/paypal/paypal.route.ts
@@ -4,6 +4,6 @@ import auth from '../../middlewares/auth';
 const router = express.Router();
 
 router.post('/pay', auth(), paypalControllers.createPaymentSession);
-router.patch('/complete-order', auth(), paypalControllers.completeOrder);
+router.patch('/complete-order', paypalControllers.completeOrder);
 
 export const paypalRouter = router;
